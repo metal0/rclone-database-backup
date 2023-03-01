db5fe5b25a1033776a6d7ec4267ebe42d45636a2
metal <metal@i0.tf>
2023-03-01 14:01:38 +0000

    list crons
    Signed-off-by: GitHub <noreply@github.com>

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -58,7 +58,8 @@ fi
 
 # configure crontab
 crontab -l | grep -q "backup.sh" && echo "cron entry exists" || echo "${CRON} cd /app/scripts && sh backup.sh > /dev/stdout" | crontab - && echo "created cron entry"
-
+echo "crons:"
+crontab -l | echo
 #
 echo "starting crond"
 crond -l 2 -f
